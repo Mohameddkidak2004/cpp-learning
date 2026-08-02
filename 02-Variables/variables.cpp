64a716a7662628d3fc4af7a1dce1b9ee5358915b
Mohameddkidak2004 <dkidak2004@gmail.com>
2026-08-02 23:50:17 +0200

    Add C++ variables example

---
+++ 02-Variables/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string name = "Moahmed";
+	int alter = 22;
+	double groesse = 1.77;
+
+  std::cout << "Name: " << name << "\n";
+	std::cout << "Alter: " << alter << "\n";
+	std::cout << "groesse: " << groesse << " Meter\n";
+
+	return 0; 
+}
