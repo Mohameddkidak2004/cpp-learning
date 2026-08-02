a632c80f4a14947eeed083f69adb91bd9c6dcc05
Mohameddkidak2004 <dkidak2004@gmail.com>
2026-08-02 23:52:23 +0200

    Add user input example

---
+++ 03-User-Input/user_input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int alter;
+
+	std::cout << "Wie alt bist du? ";
+	std::cin >> alter;
+
+	std::cout << "Du bist " << alter << " Jahre alt.\n";
+
+	return 0;
+
+}
