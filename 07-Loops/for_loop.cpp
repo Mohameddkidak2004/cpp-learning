318c3b09a424d1b63df1405dddd996cbb06278c1
Mohameddkidak2004 <dkidak2004@gmail.com>
2026-08-03 13:42:22 +0200

    Create basic for loop example

---
+++ 07-Loops/for_loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	for (int i = 1; i <= 5; i++) // For-Schleife hat drei teile (Start, Bedingung, Veraenderung)
+	{
+		std::cout << i << "\n";
+	}
+  retrun 0;
+}
